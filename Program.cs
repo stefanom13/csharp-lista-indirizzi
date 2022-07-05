67dddb591f3583d35825d6f72a68364094550111
stefanom13 <stefanomancuso13@gmail.com>
2022-07-05 14:44:08 +0200

    test push

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//In questo esercizio dovrete leggere un file CSV (che vi allego), non tanto differente da quanto visto nel live-coding in classe,
+//e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati
+//a partire dalla classe Indirizzo.
+StreamReader file = File.OpenText("C:/Users/Utente/source/repos/ListaIndirizzi/addresses.csv");
+
+
+while (!file.EndOfStream)
+{
+
+    string line = file.ReadLine();
+    Console.WriteLine(line);
+}
+file.Close();
+
